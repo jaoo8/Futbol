5784c9ba3b97a50648fca6b18b1710c987737c90
João <147158378+jaoo8@users.noreply.github.com>
2024-09-30 10:59:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'Programação',
+    'quais anos o Brasil ganhou a copa do mundo?',
+    '1958 1962 1970 1994 2002',
+)
+
+CriaCartao(
+    'Geografia',
+    'qual o maior jogador de todos os tempo que criou o simbolismo da camisa 10',
+    'Edson Arantes do Nascimento (Pelé)',
+)
+
+CriaCartao(
+    'Programação',
+    'Qual a seleção com mais titulos mundiais?',
+    'brasil',
+)
+
+CriaCartao(
+    
+    
+)
+
+CriaCartao(
+    
+)
+
+CriaCartao(
+    
+)
+
+CriaCartao(
+    
+)
+
+CriaCartao(
+    
+)
