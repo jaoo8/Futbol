a92b15a8767aa40737f6668f55c6c84e769bda5a
João <147158378+jaoo8@users.noreply.github.com>
2024-10-21 09:42:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function criaCartao(categoria, pergunta, resposta) {
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-        <p>${resposta}</p>
+        <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
         <p>${resposta}</p>
@@ -16,6 +16,7 @@ function criaCartao(categoria, pergunta, resposta) {
     `
     
     let respostaEstaVisivel = false
+
     function viraCartao(){
         respostaEstaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
--- perguntas.js
+++ perguntas.js
@@ -1,47 +1,47 @@
 criaCartao(
-    'Programação',
-    'quais anos o Brasil ganhou a copa do mundo?',
+    'Futebol',
+    'Quais anos o Brasil ganhou a copa do mundo?',
     '1958 1962 1970 1994 2002',
 )
 
-CriaCartao(
-    'Geografia',
-    'qual o maior jogador de todos os tempo que criou o simbolismo da camisa 10',
+criaCartao(
+    'Futebol',
+    'Qual o maior jogador de todos os tempo que criou o simbolismo da camisa 10',
     'Edson Arantes do Nascimento (Pelé)',
 )
 
-CriaCartao(
-    'Programação',
+criaCartao(
+    'Futebol',
     'Qual a seleção com mais titulos mundiais?',
-    'brasil',
-)
-
-CriaCartao(
-    'Geografia',
-    'Jogador com mais gols pela seleção Brasileira?',
     'Brasil',
 )
 
-CriaCartao(
-    'Programação',
+criaCartao(
+    'Futebol',
+    'Jogador com mais gols pela seleção Brasileira?',
+    'Neymar',
+)
+
+criaCartao(
+    'Futebol',
     'Quem era o jogador conhecido como “anjo das pernas tortas”?',
     'garrincha',
 )
 
-CriaCartao(
-    'Geografia',
+criaCartao(
+    'Futebol',
     'Qual o time que tem mais titulos de champions league na historia?'    ,
     'Real Madrid',
 )
 
-CriaCartao(
-    'Programação',
+criaCartao(
+    'Futebol',
     'Qual o nome do jogador deu uma cabeçada no Materazzi no final da copa de 2006',
     'Zinedine zidane',
 )
 
-CriaCartao(
-    'Geografia',
+criaCartao(
+    'Futebol',
     'Qual o jogador conhecido como “El Pistoleiro”?',
     'Luis Alberto Suárez',
 )
